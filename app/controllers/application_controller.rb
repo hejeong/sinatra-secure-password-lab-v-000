761ce6b4d16b9f23514ae4154d4103cd9de2d70a
Jonathan Hong <jonhong777@gmail.com>
2018-08-20 00:53:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    user = User.find_by(username: )
+    user = User.find_by(username: params[:username])
   end
 
   get "/failure" do
